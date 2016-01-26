f9adb8ab0e4661bdeb1855eb9c377a9c28975f85
AydenMurray97 <ayden.murray.97@gmail.com>
2016-01-26 20:22:53 +0000

    Added about page and links between about and rango

--- twd_project/Rango/views.py
+++ twd_project/Rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says 'Hello World!'")
+	return HttpResponse("Rango says : Hello World! <br/> <a href='/Rango/about'>About</a>")
+	
+def about(request):
+	return HttpResponse("Rango says that this is the about page! <br/> <a href='/Rango'>Back!</a>")
