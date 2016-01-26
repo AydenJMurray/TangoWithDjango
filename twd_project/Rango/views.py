9c05e473c27c80c81b7521d3bb69bffb7ca0a840
AydenMurray97 <ayden.murray.97@gmail.com>
2016-01-26 21:53:14 +0000

    Fixed index.html file, committed all changes

--- twd_project/Rango/views.py
+++ twd_project/Rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says : Hello World! <br/> <a href='/Rango/about'>About</a>")
+	context_dict = {'boldmessage': "I am bold font from the context!"}
+	return render(request, 'Rango/index.html', context_dict)
 	
 def about(request):
 	return HttpResponse("Rango says that this is the about page! <br/> <a href='/Rango'>Back!</a>")
