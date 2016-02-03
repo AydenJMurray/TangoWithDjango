703b34a3029de79f36a82e12073839916f82507c
AydenMurray97 <ayden.murray.97@gmail.com>
2016-02-03 16:24:03 +0000

    Started chapter 6, have to figure out what DATABASE_PATH is

--- twd_project/twd_project/settings.py
+++ twd_project/twd_project/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'twd_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': DATABASE_PATH #os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
