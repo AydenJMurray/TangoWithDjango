f7b141b8fed8f5107d969ad3abe3fe1d8b98066b
AydenMurray97 <ayden.murray.97@gmail.com>
2016-02-09 10:56:17 +0000

    Made the slugify thing

--- twd_project/Rango/admin.py
+++ twd_project/Rango/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from Rango.models import Category, Page
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_field = {'slug':('name',)}
+	
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
