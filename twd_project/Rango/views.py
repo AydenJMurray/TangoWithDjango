7d71dab30bf08d3faa92f69cba694211c87708b3
AydenMurray97 <ayden.murray.97@gmail.com>
2016-02-25 20:06:32 +0000

    Finished chapter 11?

--- twd_project/Rango/views.py
+++ twd_project/Rango/views.py
@@ -14,27 +14,30 @@ def index(request):
 	popular_pages = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': popular_pages}
 
-	visits = int(request.COOKIES.get('visits', '1'))
-
+	visits = request.session.get('visits')
+	if not visits:
+		visits = 10
+		
 	reset_last_visit_time = False
-	response = render(request, 'Rango/index.html', context_dict)
-
-	if 'last_visit' in request.COOKIES:
-		last_visit = request.COOKIES['last_visit']
+	
+	last_visit = request.session.get('last_visit')
+	if last_visit:
 		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-
-		if (datetime.now() - last_visit_time).days > 0:
+		
+		if (datetime.now() - last_visit_time).seconds > 0:
 			visits = visits + 1
 			reset_last_visit_time = True
 	else:
 		reset_last_visit_time = True
-
-		context_dict['visits'] = visits
-
-		response = render(request, 'Rango/index.html', context_dict)
+	
 	if reset_last_visit_time:
-		response.set_cookie('last_visit', datetime.now())
-		response.set_cookie('visits', visits)
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = visits
+	context_dict['visits'] = visits
+	
+	response = render(request, 'Rango/index.html', context_dict)
+	
+	return response
 
 	return response
 	
